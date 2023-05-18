a37a6e857599c639a983e058fc78be075d4eed3d
Igor Marques de Araujo <igorma.academico@gmail.com>
2023-05-18 11:17:55 -0300

    alura-mlops-deploy-modelos/

--- alura-mlops-deploy-modelos/mlops-deploy/src/app/main.py
+++ alura-mlops-deploy-modelos/mlops-deploy/src/app/main.py
@@ -3,8 +3,47 @@ from logging import BASIC_FORMAT
 
 from flask import Flask, jsonify, request
 from flask_basicauth import BasicAuth
-from mltools import MLStuff
-from simple_model import SentimentModel
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from textblob import TextBlob
+
+
+# Models
+class MLStuff:
+    def __init__(self, csv_path, target_column, test_size=0.3, 
+        random_state=123):
+        self.df = pd.read_csv(csv_path)
+        self.test_size = test_size
+        self.random_state = random_state
+        self.model = LinearRegression()
+
+        self.set_up_data(target_column)
+        
+    def set_up_data(self, target_column):
+        X = self.df.drop(target_column, axis=1).values
+        y = self.df[target_column].values
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=self.test_size, random_state=self.random_state)
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+    def model_fit(self):
+        self.model.fit(self.X_train, self.y_train)
+
+    def model_predict(self, vars):
+        return self.model.predict([vars])
+    
+class SentimentModel:
+    def __init__(self, phrase):
+        tb = TextBlob(phrase)
+        #tb = tb.translate(to='en')
+        self.tb = tb
+
+    def get_polarity(self):
+        return self.tb.sentiment.polarity
+    
 
 # Creating app
 app = Flask(__name__)
